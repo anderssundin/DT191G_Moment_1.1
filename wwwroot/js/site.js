26c422ebe36fb9e2968e110462d3986dc67b78c5
Anders Sundin <olsu2201@student.miun.se>
2024-01-16 13:35:47 +0100

    Grundläggande struktur, samtliga sidor

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Hämta in datum för att skriva ut aktuellt år till copyright
+const date = new Date();
+const copyright = date.getFullYear();
+const copyrightEl = document.getElementById('copyright');
+
+copyrightEl.innerText = copyright;
